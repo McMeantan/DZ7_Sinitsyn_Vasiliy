4a9f76f2563e56e6b9a0a75c1f421dd8f1a3482f
mcmeantan <russky1@yandex.ru>
2024-11-22 00:59:47 +0300

    Task1 added

---
+++ Task1/Program.cs
@@ -0,0 +1,39 @@
+﻿void CheckM(int m)
+{ 
+    if (m <= 0)
+    {
+        Console.WriteLine("Некорректный ввод!");
+    }
+}
+
+void CheckN(int n)
+{ 
+    if (n <= 0)
+    {
+        Console.WriteLine("Некорректный ввод!");
+    }
+}
+
+void PrintNaturalNumbers (int m, int n)
+{
+    if (m > n) return;
+
+    Console.Write(m);
+
+    if (m < n)
+    {
+        Console.Write(" ");
+    }
+
+    PrintNaturalNumbers(m + 1, n);
+}
+
+Console.WriteLine("Введите натуральное число m: ");
+int numM = Convert.ToInt32(Console.ReadLine());
+CheckM(numM);
+
+Console.WriteLine("Введите натуральное число n: ");
+int numN = Convert.ToInt32(Console.ReadLine());
+CheckN(numN);
+
+PrintNaturalNumbers(numM, numN);
