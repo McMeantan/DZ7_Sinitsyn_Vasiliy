a2a16e9dd8d3c500877136cb4a4dcab0e97b2d1a
mcmeantan <russky1@yandex.ru>
2024-11-21 23:00:36 +0300

    Task2 added

---
+++ Task2/Program.cs
@@ -0,0 +1,42 @@
+﻿void CheckM(int m)
+{ 
+    if (m < 0)
+    {
+        Console.WriteLine("Некорректный ввод!");
+    }
+}
+
+void CheckN(int n)
+{ 
+    if (n < 0)
+    {
+        Console.WriteLine("Некорректный ввод!");
+    }
+}
+        
+int AckermannFunction(int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    else if (n == 0) 
+    {
+        return AckermannFunction(m - 1, 1);
+    }
+    else 
+    {
+        return AckermannFunction(m - 1, AckermannFunction(m, n - 1));
+    }
+}
+
+Console.WriteLine("Введите неотрицательное число m: ");
+int numM = Convert.ToInt32(Console.ReadLine());
+CheckM(numM);
+
+Console.WriteLine("Введите неотрицательное число n: ");
+int numN = Convert.ToInt32(Console.ReadLine());
+CheckN(numN);
+
+int result = AckermannFunction(numM, numN);
+Console.WriteLine($"Значение функции Аккермана для ({numM}, {numN}) равно: {result}");
