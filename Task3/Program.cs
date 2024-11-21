f2b712e24a248d550e67da44b7db7fb44a4cf870
mcmeantan <russky1@yandex.ru>
2024-11-21 22:04:52 +0300

    Task3 added

---
+++ Task3/Program.cs
@@ -0,0 +1,50 @@
+﻿// Задача 3: Вывод элементов массива в обратном порядке
+// Описание: Задайте произвольный массив. Выведите его элементы, начиная с конца.
+// Используйте рекурсию и не используйте циклы.
+
+int[] CreateArrayRnd(int size, int min, int max)
+{
+    int[] array = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = rnd.Next(min, max);
+    }
+
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1)
+        {
+            Console.Write($"{array[i]}, ");
+        }
+        else
+        {
+            Console.Write($"{array[i]}");
+        }
+    }
+    Console.Write("]");
+}
+
+void ShowReverse(int[] array, int index)
+{
+    if (index < 0) return;
+
+    Console.Write($" {array[index]}");
+
+    if (index > 0) Console.Write(",");
+
+    ShowReverse(array, index - 1);
+}
+
+Console.WriteLine("Задайте размер массива");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int[] arr = CreateArrayRnd(n, -10, 10);
+PrintArray(arr);
+ShowReverse(arr, n - 1);
